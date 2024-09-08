bb95ead740d87fe8e89c06527d5bfe92f033953c
Björn Opitz <bjoern.opitz@posteo.de>
2024-09-08 12:22:43 +0000

    loading model works

--- app/model.py
+++ app/model.py
@@ -38,7 +38,44 @@ def download_artifact():
     artifact = wandb.Api().artifact(artifact_path, type="model")
     artifact.download(root=MODELS_DIR)
 
+# Do the actual download: 
+# download_artifact() # already done :-) 
 
-download_artifact()
+
+def get_raw_model() -> ResNet:
+    # overwrite final classifier layer with our own output layers
+    N_CLASSES = 6
+
+    model = resnet18(weights=None)  # not getting the weights from the original resnet18 here!
+    model.fc = nn.Sequential(       # nb: make sure that this architecture is like in the Kaggle nb!
+        nn.Linear(512, 512),
+        nn.ReLU(),
+        nn.Linear(512, N_CLASSES)
+    )
+
+    return model
+
+def load_model() -> ResNet:
+    download_artifact()
+
+    model = get_raw_model()
+    model_state_dict_path = os.path.join(MODELS_DIR, MODEL_FILE_NAME)
+    model_state_dict = torch.load(model_state_dict_path, map_location="cpu")    # loading weights _to_ the CPU.
+    # The model_state_dict contains the _weights_ from the trained model!
+    model.load_state_dict(model_state_dict, strict=True)    # nb: this was False earlier ...
+    model.eval()
+
+    return model
 
 
+def load_transforms() -> transforms.Compose:
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224), 
+        transforms.ToTensor(), 
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
+
+
+# load_model()
